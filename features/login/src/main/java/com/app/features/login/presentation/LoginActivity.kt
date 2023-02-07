278f29b363484c0d4b1bc478be3b1194bd3f8829
rodolfo <rodolforuyz@gmail.com>
2023-02-07 20:01:48 -0300

    refactor status

--- features/login/src/main/java/com/app/features/login/presentation/LoginActivity.kt
+++ features/login/src/main/java/com/app/features/login/presentation/LoginActivity.kt
@@ -20,20 +20,10 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun hideStatusBar() {
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
-        window.statusBarColor = Color.TRANSPARENT
-    }
-    private fun setWindowFlag(bits: Int, on: Boolean) {
-        val win = window
-        val winParams = win.attributes
-        if (on) {
-            winParams.flags = winParams.flags or bits
-        } else {
-            winParams.flags = winParams.flags and bits.inv()
+        with(window) {
+            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            attributes.flags = window.attributes.flags and WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS.inv()
+            statusBarColor = Color.TRANSPARENT
         }
-        win.attributes = winParams
     }
-
-
 }
