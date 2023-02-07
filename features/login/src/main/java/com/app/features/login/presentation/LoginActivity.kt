9840d7b47e2d547a161610818db136657dd0ef26
rodolfo <rodolforuyz@gmail.com>
2023-02-06 23:13:06 -0300

    removido status bar

--- features/login/src/main/java/com/app/features/login/presentation/LoginActivity.kt
+++ features/login/src/main/java/com/app/features/login/presentation/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.app.features.login.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.app.features.login.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -10,6 +11,7 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
