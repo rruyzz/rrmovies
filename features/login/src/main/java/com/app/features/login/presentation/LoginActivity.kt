eef8aa57977aedde2c213f69ed3938a1b28987f7
rodolfo <rodolforuyz@gmail.com>
2023-02-06 23:56:08 -0300

    removido status bar

--- features/login/src/main/java/com/app/features/login/presentation/LoginActivity.kt
+++ features/login/src/main/java/com/app/features/login/presentation/LoginActivity.kt
@@ -1,7 +1,10 @@
 package com.app.features.login.presentation
 
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
 import com.app.features.login.databinding.ActivityLoginBinding
 
@@ -11,8 +14,26 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        hideStatusBar()
     }
+
+    private fun hideStatusBar() {
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
+        window.statusBarColor = Color.TRANSPARENT
+    }
+    private fun setWindowFlag(bits: Int, on: Boolean) {
+        val win = window
+        val winParams = win.attributes
+        if (on) {
+            winParams.flags = winParams.flags or bits
+        } else {
+            winParams.flags = winParams.flags and bits.inv()
+        }
+        win.attributes = winParams
+    }
+
+
 }
