5de6708b3495366c5f66dcb5c1c9ea7103647a5d
rodolfo <rodolforuyz@gmail.com>
2023-03-05 17:12:18 -0300

    fonts e search

--- features/home/src/main/java/com/app/features/home/presentation/HomeActivity.kt
+++ features/home/src/main/java/com/app/features/home/presentation/HomeActivity.kt
@@ -1,7 +1,10 @@
 package com.app.features.home.presentation
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import com.app.features.home.databinding.ActivityHomeBinding
 import com.example.navigation.LoginNavigator
 import org.koin.android.ext.android.inject
@@ -18,11 +21,22 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setButton()
+        hideStatusBar()
     }
 
     private fun setButton() {
-        binding.textView.setOnClickListener {
-            viewModel.getMovies()
+//        binding.textView.setOnClickListener {
+//            viewModel.getMovies()
+//        }
+    }
+
+    private fun hideStatusBar() {
+        with(window) {
+            decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            attributes.flags =
+                window.attributes.flags and WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS.inv()
+            statusBarColor = Color.TRANSPARENT
         }
     }
 }
